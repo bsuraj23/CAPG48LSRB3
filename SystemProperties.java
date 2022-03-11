70ad2f58bc3ff17f1260b1f2b5e2adbead3971cf
NagaSaiRakshith2905 <sairakshith2905@gmail.com>
2022-03-11 21:23:01 +0530

    Added SystemProperties java file

---
+++ SystemProperties.java
@@ -0,0 +1,11 @@
+//Naga Sai Rakshith B S [CAPG48LSRB346]
+public class SystemProperties {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.arch"));
+        System.out.println(System.getProperty("sun.arch.data.model"));
+        System.out.println(System.getProperty("os.version"));
+        System.out.println(System.getProperty("java.class.path"));
+        System.out.println(System.getProperty("java.version"));
+    }
+}
